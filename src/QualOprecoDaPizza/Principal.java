ba5136bb5382e0a968d91e4c5390e9891bd56ea5
Webert Maximiano <webertcoach@gmail.com>
2023-04-12 23:03:17 -0300

    Atualizando pastas

--- src/QualOprecoDaPizza/Principal.java
+++ src/QualOprecoDaPizza/Principal.java
@@ -6,8 +6,10 @@ public class Principal {
         Pizza pizza1 = new Pizza(new String[]{"Queijo", "Tomate"});
         Pizza pizza2 = new Pizza(new String[]{"Queijo", "Tomate", "Ovo", "Mussarela"});
         Pizza pizza3 = new Pizza(new String[]{"Bacon", "Milho", "Catupiry", "Molho de Tomate", "Azeitona", "Cebola"});
+
         //criando o carrinho de compra
         CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
         //adicionando as pizzas criadas no carrinho
         carrinhoDeCompras.adicionaPizza(pizza1);
         carrinhoDeCompras.adicionaPizza(pizza2);
@@ -15,8 +17,10 @@ public class Principal {
 
         //exibi o Valor total do Carrinho
         System.out.println("Valor total: R$ " + carrinhoDeCompras.getValorTotal());
+
         // exibi os ingridientes
         System.out.println("Ingredientes utilizados:");
+
         //exibi ingridientes e quantidade utilizados
         for (String ingrediente : Pizza.ingredientesUtilizados.keySet()) {
             System.out.println(ingrediente + ": " + Pizza.ingredientesUtilizados.get(ingrediente));
